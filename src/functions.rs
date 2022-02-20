23b7cd904992ebf6a449cab617dec51df7fa1a55
jeffersonfparil <jeffersonparil@gmail.com>
2022-02-20 14:27:40 +1100

    will do window struct next

--- src/functions.rs
+++ src/functions.rs
@@ -8,25 +8,10 @@ use std::fs::File;
 use std::io::{Result, Lines, BufRead, BufReader};
 use std::path::Path;
 
-pub fn stream_file<P>(filename:P) -> Result<Lines<BufReader<File>>>
-where P:AsRef<Path>, {
+pub fn stream_file<P>(filename:P) -> Result<Lines<BufReader<File>>> where P:AsRef<Path> {
     let file = File::open(filename).expect("File not found!");
     Ok(BufReader::new(file).lines())
 }
-
-// pub fn print_line_by_line(stream:Result<Lines<BufReader<File>>>){
-//     for line in stream.unwrap() {
-//         println!("{}", line.unwrap());
-//     }
-// }
-
-// pub fn print_column_by_column_per_line(stream:Result<Lines<BufReader<File>>>){
-//     for line in stream.unwrap() {
-//         for column in line.unwrap().split_whitespace() {
-//             println!("{}", column);
-//         }
-//     }
-// }
 
 fn parse_read(read:&str, reference_allele:char, quality:&str, minimum_quality:u8) -> (u64, u64, u64, u64, u64, u64, u64) {
     // qualities
@@ -44,21 +29,22 @@ fn parse_read(read:&str, reference_allele:char, quality:&str, minimum_quality:u8
     let mut I:u64 = 0;
     let mut D:u64 = 0;
     let mut N:u64 = 0;
-    let mut i:usize = 0;
+    let mut i:u64 = 0;
     let mut q:u8;
     let mut read_start_marker:bool = false;
     let mut read_regex_marker:bool = false;
     let mut insertion_deletion_char:char = '+';
     let mut insertion_deletion_count:u32 = 0;
     let mut insertion_deletion_count_place_value:u32 = 1;
-
+    // for debugging
     let mut n_read_start = 0;
     let mut n_read_end = 0;
-
+    // iterate across reads
+    println!("qual length: {}", qual.iter().count());
+    println!("read length: {}", vec.iter().count());
     for x in vec.iter(){
-        q = qual[i];
+        q = qual[i as usize];
         if *x == '^' {
-            // println!("x = {}", *x);
             read_start_marker = true;
             n_read_start = n_read_start + 1;
             continue;
@@ -86,51 +72,78 @@ fn parse_read(read:&str, reference_allele:char, quality:&str, minimum_quality:u8
                     insertion_deletion_count = (insertion_deletion_count*insertion_deletion_count_place_value)
                                                 + d;
                     insertion_deletion_count_place_value = insertion_deletion_count_place_value * 10;
+                } else if insertion_deletion_count > 1 {
+                    match insertion_deletion_char {
+                        '+' => I = I + 1,
+                        '-' => D = D + 1,
+                        _ => N = N + 1
+                    }
+                    insertion_deletion_count = insertion_deletion_count - 1;
                 } else {
                     match insertion_deletion_char {
-                        '+' => I = I + (insertion_deletion_count as u64),
-                        '-' => D = D + (insertion_deletion_count as u64),
-                        _ => N = N + (insertion_deletion_count as u64),
+                        '+' => I = I + 1,
+                        '-' => D = D + 1,
+                        _ => N = N + 1
                     }
                     read_regex_marker = false;
-                    insertion_deletion_count_place_value = 1;
+                    insertion_deletion_count = 0;
+                    insertion_deletion_count_place_value = 0;
                 }
+                i = i - 1;
             } else {
                 match *x {
                     'A' | 'a' => A = A + 1,
                     'T' | 't' => T = T + 1,
-                    'C' | 'c'  => C = C + 1,
+                    'C' | 'c' => C = C + 1,
                     'G' | 'g' => G = G + 1,
+                    '*'       => D = D + 1,
                     _ => N = N + 1
                 }
             }
             i = i + 1;
-        } else {
-            i = i + 1;
         }
     }
-    println!("Read starts: {}", n_read_start);
-    println!("Read ends: {}", n_read_end);
-    println!("i went up to: {}", i);
-    println!("qual length: {}", qual.iter().count());
-    println!("read length: {}", vec.iter().count());
+    // debugging
+    // println!("Read starts: {}", n_read_start);
+    // println!("Read ends: {}", n_read_end);
+    // println!("i went up to: {}", i);
+    // println!("qual length: {}", qual.iter().count());
+    // println!("read length: {}", vec.iter().count());
+    // output tupe of counts
     (A, T, C, G, I, D, N)
 }
 
-
-pub fn parse_line(stream:Result<Lines<BufReader<File>>>){
+pub fn iterate_across_lines(stream:Result<Lines<BufReader<File>>>){
+    let min_base_quality: u8 = 20;
     let mut vec: Vec<String>;
+    let mut counts: Vec<u64>;
     for line in stream.unwrap() {
         vec = line
                 .unwrap()
                 .split_whitespace()
                 .map(str::to_string)
                 .collect();
-        // println!("{}", vec.iter().count());
-        // println!("{}", vec[4]);
-        // let chars: Vec<char> = vec[4].chars().collect();
-        // println!("{}", chars[4]);
-        let (a, t, c, g, i, d, n) = parse_read(&vec[4], 'A', &vec[5], 0);
-        println!("{}", n);
+        let (chromosome, position, reference_allele, counts) = parse_line(vec, min_base_quality);
+        println!("{}", chromosome);
+        println!("{}", position);
+        println!("{}-{}-{}-{}-{}-{}-{}<--->{}-{}-{}-{}-{}-{}-{}",
+                 counts[0], counts[1], counts[2], counts[3], counts[4], counts[5], counts[6],
+                 counts[7], counts[8], counts[9], counts[10], counts[11], counts[12], counts[13]
+                );
     }
 }
+
+pub fn parse_line(vec: Vec<String>, min_base_quality: u8) -> (String, u64, char, Vec<u64>) {
+    let mut chromosome: String = vec[0].to_string();
+    let mut position: u64 = vec[1].parse().unwrap();
+    let mut reference_allele: char = vec[2].chars().next().expect("We're expecting a single character as reference allele!");
+    let p: usize = vec.iter().count();
+    let mut out: Vec<u64> = vec![];
+    for i in (4..p).step_by(3) {
+        println!("{}", i);
+        let (A, T, C, G, I, D, N) = parse_read(&vec[i], reference_allele, &vec[i+1], min_base_quality);
+        out.push(A); out.push(T); out.push(C); out.push(G); out.push(I); out.push(D); out.push(N);
+    }
+    return (chromosome, position, reference_allele, out);
+}
+
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,6 @@ fn main() {
     let stream = functions::stream_file("res/test.pileup");
     // functions::print_line_by_line(stream);
     // functions::print_column_by_column_per_line(stream);
-    functions::parse_line(stream);
+    functions::iterate_across_lines(stream);
 
 }
